16c92e825b45232d4ac164d6f0aa093fa2ec8a39
Luis G Palomares <luisgpalomares@Luis-GPs-MacBook-Pro.local>
2020-03-22 12:50:28 -0700

    cambios

--- application/views/plan/status.php
+++ application/views/plan/status.php
@@ -1,9 +1,7 @@
 <html>
-
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
-
 <body>
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -29,6 +27,7 @@
               <a class="dropdown-item" href="#">Another action</a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item" href="#">Another thingy</a>
             </div>
           </li>
           <li class="nav-item">
