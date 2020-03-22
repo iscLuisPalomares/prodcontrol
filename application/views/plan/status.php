3a9dc34d3e1a6636f4c9ca066a2625f9ca707b75
Luis Gerardo Palomares Ramos <luy_a7x@hotmail.com>
2020-03-22 15:22:58 +0000

    amonos

--- application/views/plan/status.php
+++ application/views/plan/status.php
@@ -2,16 +2,39 @@
 
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <style>
+    .sidebarbutton {
+      height: 50px;
+      transition: 0.3s;
+      background: #25a0a8;
+      border-radius: 5px;
+      text-align: center;
+      vertical-align: middle;
+      margin: 2px;
+    } 
+    .sidebarbutton:hover {
+      background: #2bbbc4;
+      transition: 0.3s;
+      transform: scale(1.05);
+    } 
+    .fuenteblanca {
+      color: #fff;
+    }
+    
+  </style>
 </head>
 
 <body>
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
       <div class="col-sm-12 col-md-4 col-lg-2">
         <div class="col-sm-12">
-          <img src="" alt="..." class="img-thumbnail">
+          <img src="https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" alt="imagen" class="rounded img-thumbnail">
         </div>
-        <div></div>
+        <div class="sidebarbutton fuenteblanca">Ver plan</div>
+        <div class="sidebarbutton fuenteblanca">Comprar plan</div>
+        <div class="sidebarbutton fuenteblanca">Monitoriear Plan</div>
+        <div class="sidebarbutton fuenteblanca">Dashboard</div>
         <div></div>
         <div></div>
       </div>
@@ -27,6 +50,7 @@
   </div>
 
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </html>
