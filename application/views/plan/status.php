21a433a100fac36291505e22ea167dd3820fc4d8
Luis G Palomares <luisgpalomares@Luis-GPs-MacBook-Pro.local>
2020-03-29 11:54:31 -0700

    listo

--- application/views/plan/status.php
+++ application/views/plan/status.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?php echo base_url('assets_local/css/site.css'); ?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/site.css'); ?>">
 </head>
 <body>
   <div class="container-fluid">
